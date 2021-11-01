1494300ae0ede165fa8cb2b431e2368948e67a0c
GiPhantom <f4ringeit@yandex.ru>
2021-11-01 20:45:12 +0000

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 	<h1>Test of revert</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+<p>
+Ferst
+Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
